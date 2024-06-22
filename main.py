5b96a68197af6bee4aea8cc48d3974d5626c4481
Muazzam2006 <123855306+Muazzam2006@users.noreply.github.com>
2024-06-22 23:20:17 +0500

    Update2 main.py

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
-
-class Character(BaseModel):
-    id: int
-    name: str
-    strength: int = 0
-    agility: int = 0
-    stamina: int = 0
-    level: int = 1
-    availablePoints: int = 5
-
-class AttributeAdjustment(BaseModel):
-    strength: int
-    agility: int
-    stamina: int
-
 
 characters = [
     {
@@ -30,28 +15,41 @@ characters = [
     }
 ]
 
+class Character(BaseModel):
+    id: int = len(characters) + 1
+    name: str
+    strength: int = 0
+    agility: int = 0
+    stamina: int = 0
+    level: int = 1
+    availablePoints: int = 5
+
+class AttributeAdjustment(BaseModel):
+    strength: int
+    agility: int
+    stamina: int
+
 @app.get("/api/characters/{id}")
-async def get_character(id: int):
-    for character in characters:
-        if character.get("id") == id:
-            return character
-    return {"response": "Character not found"}
+def get_character(id: int):
+    if id not in characters:
+        raise HTTPException(status_code=404, detail="Character not found")
+    return characters[id]
 
 @app.post("/api/characters")
-async def create_character(character: Character):
-    characters.append(character)
-    return characters
+async def create_character(name_: str):
+    ch = Character(name=name_)
+    characters.append(ch)
+    return ch
 
 @app.put("/api/characters/{g_id}/attributes")
 async def adjust_character_attributes(g_id: int, adjustments: AttributeAdjustment):
     character = characters[g_id]
     if g_id not in characters:
-        return {"response": "Character not found"}
+        raise HTTPException(status_code=404, detail="Character not found")
     if (adjustments.strength + adjustments.agility + adjustments.stamina) > character.availablePoints:
-        return {"response": "Not enough available points"}
+        raise HTTPException(status_code=400, detail="Not enough available points")
     character.strength = adjustments.strength
     character.agility = adjustments.agility
     character.stamina = adjustments.stamina
     character.availablePoints -= (adjustments.strength + adjustments.agility + adjustments.stamina)
     return character
-
