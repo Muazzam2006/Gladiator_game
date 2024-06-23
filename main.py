f7898f4180c36325279d19c4cf590ab3bc7aa941
Muazzam2006 <123855306+Muazzam2006@users.noreply.github.com>
2024-06-23 11:14:41 +0500

    Update3.0 main.py

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ characters = [
         "stamina": 0, 
         "level": 1, 
         "availablePoints": 5
+    },
+    {
+        "id": 2,
+        "name": "charac2",
+        "strength": 0,
+        "agility": 0,
+        "stamina": 0,
+        "level": 1,
+        "availablePoints": 5
     }
 ]
 
@@ -29,11 +38,13 @@ class AttributeAdjustment(BaseModel):
     agility: int
     stamina: int
 
+
 @app.get("/api/characters/{id}")
 def get_character(id: int):
-    if id not in characters:
+    chc = list(filter(lambda ch: ch.get("id") == id, characters))
+    if not chc:
         raise HTTPException(status_code=404, detail="Character not found")
-    return characters[id]
+    return chc[0]
 
 @app.post("/api/characters")
 async def create_character(name_: str):
@@ -41,15 +52,18 @@ async def create_character(name_: str):
     characters.append(ch)
     return ch
 
-@app.put("/api/characters/{g_id}/attributes")
-async def adjust_character_attributes(g_id: int, adjustments: AttributeAdjustment):
-    character = characters[g_id]
-    if g_id not in characters:
+@app.put("/api/characters/{id}/attributes")
+async def adjust_character_attributes(id: int, adjustments: AttributeAdjustment):
+    chc = list(filter(lambda ch: ch.get("id") == id, characters))
+    if not chc:
         raise HTTPException(status_code=404, detail="Character not found")
-    if (adjustments.strength + adjustments.agility + adjustments.stamina) > character.availablePoints:
-        raise HTTPException(status_code=400, detail="Not enough available points")
-    character.strength = adjustments.strength
-    character.agility = adjustments.agility
-    character.stamina = adjustments.stamina
-    character.availablePoints -= (adjustments.strength + adjustments.agility + adjustments.stamina)
-    return character
+    else:
+        chc = chc[0]
+        if (adjustments.strength + adjustments.agility + adjustments.stamina) > chc["availablePoints"]:
+            raise HTTPException(status_code=400, detail="Not enough available points")
+        else:
+            chc["strength"] = adjustments.strength
+            chc["agility"] = adjustments.agility
+            chc["stamina"] = adjustments.stamina
+            chc["availablePoints"] -= (adjustments.strength + adjustments.agility + adjustments.stamina)
+            return chc
